01d240e4e47194fe3f746f8bbc584873b353d487
Aben Princely <abenprincely1@gmail.com>
2025-07-22 04:25:41 +0200

    setting back render without postgre

--- backend/settings.py
+++ backend/settings.py
@@ -1,6 +1,5 @@
-import os
-import dj_database_url
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -50,7 +49,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 AUTH_USER_MODEL = 'api.User'
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'feedback_db',
+        'USER': 'myuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5433',
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = []
@@ -61,6 +67,5 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
